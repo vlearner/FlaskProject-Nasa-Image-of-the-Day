2f4c999e84c887e3d7a6b97b92cb7d187a8584b7
vishalbarot <vishalbarot@ymail.com>
2023-03-19 09:24:21 -0500

    Get form value

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def flask_project():  # put application's code here
     return render_template("index.html")
+
+
+@app.route('/name', methods=['POST'])
+def name_page():
+    return render_template(
+        "name.html",
+        name=request.form['name'],
+        last_name=request.form['last_name']
+    )
 
 
 if __name__ == '__main__':
